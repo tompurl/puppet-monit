b17d959aa7a5a8adaac425fc9caa41ea66d24275
Tom Purl <tom@tompurl.com>
2014-08-01 20:43:06 -0500

    Added a bunch of new parameters to support smtp

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,6 +15,11 @@
 #
 
 class monit ($alert_address,
+             $smtp_host,
+             $smtp_port,
+             $smtp_user_name,
+             $smtp_password,
+             $email_hostname,
              $web_username = "admin",
              $web_password = "monit") {
     include monit::install, monit::config, monit::service
