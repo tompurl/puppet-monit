04a9a8d067a941aeab7288fb1b881fa82e047005
Tom Purl <tom@tompurl.com>
2014-08-24 14:12:23 -0500

    Got rid of an unnecessary parameter.

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,6 @@ class monit ($alert_address,
              $smtp_port,
              $smtp_user_name,
              $smtp_password,
-             $email_hostname,
              $web_username = "admin",
              $web_password = "monit") {
     include monit::install, monit::config, monit::service
